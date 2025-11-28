b537d171ab301d63a0bf09b9cca02081ca7ee152
minjeeki <minjeeki@student.42seoul.kr>
2025-11-28 17:47:22 +0900

    fix: add tailwind css plugins

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
@@ -7,6 +8,7 @@ export default defineConfig({
   base: '/front_7th_chapter3-1/',
   plugins: [
     react(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
