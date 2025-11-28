ec895c27a0e17821745db18818630a039fa3df8b
minjeeki <minjeeki@student.42seoul.kr>
2025-11-28 16:50:13 +0900

    fix: build 통과를 위한 vite.config.ts 파일 수정

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -1,21 +1,23 @@
-import { defineConfig } from 'vitest/config';
-import tailwindcss from '@tailwindcss/vite';
-// @ts-expect-error - @vitejs/plugin-react types are not properly exported
-import react from '@vitejs/plugin-react';
-import * as path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: '/front_7th_chapter3-1/',
+  plugins: [
+    react(),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
+  // @ts-ignore - Vitest config
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
     css: true,
-  }
-});
+  },
+})
